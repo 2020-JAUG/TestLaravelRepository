c5a60d93efa4fd6b1388f660f243688cd8da98dc
Alex <aurbina@aiudo.es>
2021-11-18 17:15:26 +0100

    Add the group path with middleware

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Helpers\RoutesHelper;
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,15 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     // return $request->user();
 // });
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);  
+});
+
 
 Route::group(['prefix' => 'user'],function(Router $router)
 {
