fd175dae8732446d8ac65f1b4d7852b75a287be4
Alex <aurbina@aiudo.es>
2021-11-18 17:17:21 +0100

    Define the methods of JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,5 +46,25 @@ class User extends Authenticatable
     public function customer(): HasMany
     {
         return $this->hasMany(Customer::class);
+
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
